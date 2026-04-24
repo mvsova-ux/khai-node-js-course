064a4624374a042b102e9d79437671bc93f2d400
mvsova-ux <m.v.sova@student.khai.edu>
2026-04-24 19:53:58 +0200

    create products module and update user routes

--- src/user/user.router.js
+++ src/user/user.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const db = require('../db');
-const { users } = require('../db/schema');
 const router = express.Router();
+const { eq } = require('drizzle-orm');
+const { users, products } = require('../db/schema');
 
-// handle get request for path /users
+
 router.post('/users', async (request, response) => {
     const { body } = request;
     await db.insert(users).values(body);
@@ -13,6 +14,14 @@ router.post('/users', async (request, response) => {
 router.get('/users', async (request, response) => {
     const users = await db.query.users.findMany();
     return response.json(users);
+});
+
+router.get('/users/:id/products', async (request, response) => {
+    const { id } = request.params;
+    const userProdusts = await db.query.products.findMany({
+        where: eq(products.userId, +id)
+    });
+    return response.json(userProdusts);
 });
 
 module.exports = router;
